206b010a1edcad9e8f5e12d702d5c44c23caa566
msumana <82926968+msumana@users.noreply.github.com>
2024-08-14 16:26:44 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -6,3 +6,10 @@ def add(a, b):
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+def subtract(a, b):
+    return a - b
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(1, -1) == 2
